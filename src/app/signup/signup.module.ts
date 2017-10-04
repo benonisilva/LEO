ea1d693a004a6a56c1ac8b03851c1f70a29adffa
benoni <benoni.silva@gmail.com>
2017-10-04 11:20:08 -0300

    init

--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { SignupRoutingModule } from './signup-routing.module';
 import { SignupComponent } from './signup.component';
+import {
+  ModalComponent
+} from '../layout/bs-component/components';
 
 @NgModule({
   imports: [
